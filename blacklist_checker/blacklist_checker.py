5357cd8a906cf1f0a6dee8c97cad9100a5a3461b
Pawel Cygal <root@hetakte>
2018-04-02 13:23:27 +0200

    added simple usage function

--- blacklist_checker/blacklist_checker.py
+++ blacklist_checker/blacklist_checker.py
@@ -6,32 +6,40 @@ import sys
 import ConfigParser
 import ast
 
+def usage():
+    print 'Usage: %s <ip>\n' %(sys.argv[0])
+
 
 def get_config():
     CONFIG = ConfigParser.RawConfigParser(allow_no_value=True)
-    CONFIG.read('blacklists.cfg')
-    bls = ast.literal_eval(CONFIG.get('blacklist_config', 'blacklist'))
-    return bls
+    try:
+        CONFIG.read('blacklists.cfg')
+        blacklist = ast.literal_eval(CONFIG.get('blacklist_config', 'blacklist'))
+    except (IOError,ConfigParser.NoSectionError):
+        print "ERR: Somthing went wrong. Looks like configuration file is missing or has wrong structure" 
+    else:
+        return blacklist
 
 
-def check_blacklists(bls):
-    for bl in bls:
-        try:
-            my_resolver = dns.resolver.Resolver()
-            query = '.'.join(reversed(str(myIP).split("."))) + "." + bl
-            answers = my_resolver.query(query, "A")
-            answer_txt = my_resolver.query(query, "TXT")
-            print 'IP: %s IS listed in %s (%s: %s)' %(myIP, bl, answers[0], answer_txt[0])
-        except dns.resolver.NXDOMAIN:
-            print 'IP: %s is NOT listed in %s' %(myIP, bl)
+def check_blacklists(blacklist):
+    if blacklist:
+        for blacklist_provider in blacklist:
+            try:
+                my_resolver = dns.resolver.Resolver()
+                query = '.'.join(reversed(str(myIP).split("."))) + "." + blacklist_provider
+                answers = my_resolver.query(query, "A")
+                answer_txt = my_resolver.query(query, "TXT")
+                print 'IP: %s IS listed in %s (%s: %s)' %(myIP, blacklist_provider, answers[0], answer_txt[0])
+            except dns.resolver.NXDOMAIN:
+                print 'IP: %s is NOT listed in %s' %(myIP, blacklist_provider)
 
 
 if len(sys.argv) != 2:
-    print 'Usage: %s <ip>' %(sys.argv[0])
+    usage()
     quit()
 
 myIP = sys.argv[1]
 
 if __name__ == '__main__':
-    bls = get_config()
-    check_blacklists(bls)
+    blacklist = get_config()
+    check_blacklists(blacklist)
