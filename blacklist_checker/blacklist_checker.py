7828cb9b44155c4897ad6e848a4628dceb8ff7aa
Pawel Cygal <root@hetakte>
2018-04-02 13:39:04 +0200

    added docstrings

--- blacklist_checker/blacklist_checker.py
+++ blacklist_checker/blacklist_checker.py
@@ -6,22 +6,26 @@ import sys
 import ConfigParser
 import ast
 
+
 def usage():
-    print 'Usage: %s <ip>\n' %(sys.argv[0])
+    """just prints help"""
+    print 'Usage: %s <ip>\n' % (sys.argv[0])
 
 
 def get_config():
+    """Function is checking if configuration file exist and has required section"""
     CONFIG = ConfigParser.RawConfigParser(allow_no_value=True)
     try:
         CONFIG.read('blacklists.cfg')
         blacklist = ast.literal_eval(CONFIG.get('blacklist_config', 'blacklist'))
-    except (IOError,ConfigParser.NoSectionError):
+    except (IOError, ConfigParser.NoSectionError):
         print "ERR: Somthing went wrong. Looks like configuration file is missing or has wrong structure" 
     else:
         return blacklist
 
 
 def check_blacklists(blacklist):
+    """Function is checking if your ip is listed on mailing blacklist configured in blacklist.cfg"""
     if blacklist:
         for blacklist_provider in blacklist:
             try:
@@ -29,9 +33,9 @@ def check_blacklists(blacklist):
                 query = '.'.join(reversed(str(myIP).split("."))) + "." + blacklist_provider
                 answers = my_resolver.query(query, "A")
                 answer_txt = my_resolver.query(query, "TXT")
-                print 'IP: %s IS listed in %s (%s: %s)' %(myIP, blacklist_provider, answers[0], answer_txt[0])
+                print 'IP: %s IS listed in %s (%s: %s)' % (myIP, blacklist_provider, answers[0], answer_txt[0])
             except dns.resolver.NXDOMAIN:
-                print 'IP: %s is NOT listed in %s' %(myIP, blacklist_provider)
+                print 'IP: %s is NOT listed in %s' % (myIP, blacklist_provider)
 
 
 if len(sys.argv) != 2:
